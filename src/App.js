376a3daed3cc77dc1e1c3d7f46de2383ee2ce2c7
Joseph Stoops <Joseph.Stoops@FamilySearch.org>
2019-09-09 08:30:52 -0600

    Make the state and dispatch of the reducer part of the context. Refactors a lot of the jQuery to be Reacty.

--- src/App.js
+++ src/App.js
@@ -4,29 +4,41 @@ import './styles.css'
 
 function mineFieldReducer(state, action) {
 	switch (action.type) {
-		case 'size:Beginner':
+		case 'newGame:Beginner':
 			return Object.assign({}, state, {
+				status: 'Playing',
 				height: 9,
 				width: 9,
-				mines: 10
+				mines: 10,
+				minesLeft: 10,
+				minesTotal: 10
 			})
-			case 'size:Intermediate':
+			case 'newGame:Intermediate':
 				return Object.assign({}, state, {
-					height: 9,
-					width: 9,
-					mines: 10
+					status: 'Playing',
+					height: 16,
+					width: 16,
+					mines: 40,
+					minesLeft: 40,
+					minesTotal: 40
 				})
-			case 'size:Expert':
+			case 'newGame:Expert':
 				return Object.assign({}, state, {
-					height: 9,
-					width: 9,
-					mines: 10
+					status: 'Playing',
+					height: 16,
+					width: 30,
+					mines: 99,
+					minesLeft: 99,
+					minesTotal: 99
 				})
-		case 'size:Custom':
+		case 'newGame:Custom':
 			return Object.assign({}, state, {
+				status: 'Playing',
 				height: action.height,
 				width: action.width,
-				mines: action.mines
+				mines: action.mines,
+				minesLeft: action.mines,
+				minesTotal: action.mines
 			})
 		case 'mapStyle:Classic':
 			return Object.assign({}, state, {mapStyle: ''})
@@ -34,6 +46,8 @@ function mineFieldReducer(state, action) {
 			return Object.assign({}, state, {mapStyle: 'gr-'})
 		case 'game:Status':
 			return Object.assign({}, state, action.status)
+		case 'game:minesLeft':
+			return Object.assign({}, state, {minesLeft: action.minesLeft })
 		case 'game:Reset':
 			return Object.assign({}, state, {
 				status: 'Idle',
@@ -46,31 +60,48 @@ function mineFieldReducer(state, action) {
 		case 'player:money': // TODO
 		default:
 			return {
+				// GameField
 				mapStyle: 'Classic',
+				status: 'Idle',
 				height: 9,
 				width: 9,
-				mines: 10
+				mines: 10,
+				minesLeft: 10,
+				minesTotal: 10,
+
+				// Player
+				money: 0,
+				wins: 0,
+				losses: 0,
+				flaggedMines: 0
 			}
 	}
 }
 
-export const ThemeContext = createContext();
+export const GameContext = createContext();
 
 export default function App() {
 	const [state, dispatch] = useReducer(mineFieldReducer, {
 		mapStyle: 'Classic',
+		status: 'Idle',
 		height: 9,
 		width: 9,
-		mines: 10
+		mines: 10,
+		minesLeft: 10,
+		minesTotal: 10,
+		money: 0,
+		wins: 0,
+		losses: 0,
+		flaggedMines: 0
 	})
 
   return (
-		<ThemeContext.Provider value={state.mapStyle}>
+		<GameContext.Provider value={{ state, dispatch }}>
 			<div id="wrapper">
 				<h1>M.I.N.E. - Mine Initiative of Nations Everywhere</h1>
-				<Menu dispatch={dispatch} />
-				<MineField state={state} />
+				<Menu />
+				<MineField />
 			</div>
-		</ThemeContext.Provider>
+		</GameContext.Provider>
   );
 }
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
+import { GameContext } from '../App'
 
-export default function Menu(props) {
-	const { dispatch } = props
+export default function Menu() {
+  const { dispatch, state } = useContext(GameContext)
+	const { flaggedMines, height, losses, mapStyle, mines, minesLeft, minesTotal, money, status, width, wins } = state
 	const [difficulty, setDifficulty] = useState('Beginner')
 	const [customField, setCustomField] = useState({height: 9, mines: 10, width: 9})
-	const [mapStyle, setMapStyle] = useState('Classic')
-	if (!dispatch) return false
+	const [_mapStyle, setMapStyle] = useState('Classic')
 
 	function startNewGame() {
-		const action = { type: `size:${difficulty}` }
+		const action = { type: `newGame:${difficulty}` }
 		if (difficulty === 'Custom') {
 			Object.assign(action, customField)
 		}
 		dispatch(action)
 
 		// Set mapStyle
-		action.type = mapStyle
+		action.type = `mapStyle:${mapStyle}`
 		dispatch(action)
 	}
 
@@ -26,17 +27,16 @@ export default function Menu(props) {
 	return (
 		<aside id="gameMenu">
 			<h4>Player Stats</h4>
-			Player: <span id="playerName"></span> <br />
-			Wallet: $<span id="playerWallet"></span> <br />
-			Wins: <span id="playerWins"></span> <br />
-			Losses: <span id="playerLosses"></span> <br />
-			Flagged mines: <span id="playerFlaggedMines"></span> <br />
+			Wallet: $<span id="playerWallet">${money}</span> <br />
+			Wins: <span id="playerWins">{wins}</span> <br />
+			Losses: <span id="playerLosses">{losses}</span> <br />
+			Flagged mines: <span id="playerFlaggedMines">{flaggedMines}</span> <br />
 
 			<br />
 			<h4>Game Stats</h4>
-			Mines Total: <span id="minesTotal"></span> <br />
-			Mines Left: <span id="minesLeft"></span> <br />
-			Status: <span id="gameState"></span>
+			Mines Total: <span id="minesTotal">{ minesTotal }</span> <br />
+			Mines Left: <span id="minesLeft">{ minesLeft }</span> <br />
+			Status: <span id="gameState">{ status }</span>
 
 			<br /><br />
 			<h4>Game Menu</h4>
@@ -53,8 +53,8 @@ export default function Menu(props) {
 			</div>
 			<br />
 			Map Style:<br />
-			<input id="Classic" type="radio" name="mineField" value="Classic" onChange={event => setMapStyle(event.target.value)} checked={mapStyle === 'Classic'} /><label htmlFor="Classic">Classic</label><br />
-			<input id="Grassy" type="radio" name="mineField" value="Grassy" onChange={event => setMapStyle(event.target.value)} checked={mapStyle === 'Grassy'} /><label htmlFor="Grassy">Grassy</label><br />
+			<input id="Classic" type="radio" name="mineField" value="Classic" onChange={event => setMapStyle(event.target.value)} checked={_mapStyle === 'Classic'} /><label htmlFor="Classic">Classic</label><br />
+			<input id="Grassy" type="radio" name="mineField" value="Grassy" onChange={event => setMapStyle(event.target.value)} checked={_mapStyle === 'Grassy'} /><label htmlFor="Grassy">Grassy</label><br />
 
 			<button id="bNewGame" onClick={startNewGame}>Start New Game!</button> <br />
 			<button id="bSaveGame">Save Game</button> <br />
